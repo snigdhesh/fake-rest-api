ff634f29410fee794e950c14e5404e0395b6c530
snigdhesh <snigdhesh@live.com>
2024-04-08 10:36:26 -0400

    Updated CORS config

--- src/main/java/com/example/fakerestapi/config/WebConfig.java
+++ src/main/java/com/example/fakerestapi/config/WebConfig.java
@@ -1,16 +1,23 @@
 package com.example.fakerestapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//This class enables CORS from any origin
 @Configuration
 @EnableWebMvc
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
