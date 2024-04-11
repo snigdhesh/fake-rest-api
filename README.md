82e6318ecb114283674fe5f5614bddf14d388bc2
snigdhesh <snigdhesh@live.com>
2024-04-11 10:13:22 -0400

    Added sqls

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 This is a fake rest API
 
 `data.sql` and `schema.sql` under
-`src/main/resources` can be enabled.
+`src/main/resources` can be deleted.
 These sql files are to initialize table with sample data
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
--- CREATE TABLE IF NOT EXISTS groceries(
---     `id` bigint(20) NOT NULL AUTO_INCREMENT,
---     `name` varchar(255) DEFAULT NULL,
---     `price` number(255) DEFAULT NULL,
---     PRIMARY KEY (`id`)
--- )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS sandbox.groceries(
+`id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) DEFAULT NULL,
+    `price` decimal(6,2) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
